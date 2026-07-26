bf028911a7b3953d4736f54f13db65820a3d3188
nguyentiep91 <149965025+nguyentiep91@users.noreply.github.com>
2026-07-26 17:47:55 +0700

    feat: add activation handler

---
+++ includes/class-nt-content-images-activator.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin activation handler.
+ *
+ * @package NT_Content_Images
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+final class NT_Content_Images_Activator {
+	/**
+	 * Sets initial non-sensitive options.
+	 */
+	public static function activate(): void {
+		if ( version_compare( PHP_VERSION, '8.1', '<' ) ) {
+			deactivate_plugins( plugin_basename( NT_CONTENT_IMAGES_FILE ) );
+			wp_die(
+				esc_html__( 'NT – Tạo ảnh cho nội dung WordPress yêu cầu PHP 8.1 trở lên.', 'nt-tao-anh-noi-dung-wordpress' )
+			);
+		}
+
+		add_option( 'nt_content_images_version', NT_CONTENT_IMAGES_VERSION, '', false );
+		add_option( 'nt_content_images_workflow_mode', 'draft_review', '', false );
+		add_option( 'nt_content_images_delete_data_on_uninstall', 'no', '', false );
+	}
+}
