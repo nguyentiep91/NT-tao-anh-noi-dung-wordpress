2b1468522c2db2d762692ec4c115606b95564fcf
nguyentiep91 <149965025+nguyentiep91@users.noreply.github.com>
2026-07-26 23:27:45 +0700

    Mở rộng contract nhà cung cấp ảnh

--- includes/providers/interface-nt-content-images-image-provider.php
+++ includes/providers/interface-nt-content-images-image-provider.php
@@ -12,7 +12,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 interface NT_Content_Images_Image_Provider_Interface {
 	public function get_id(): string;
 
+	public function get_label(): string;
+
 	public function is_configured(): bool;
+
+	/**
+	 * Returns models that can be selected in the administration screen.
+	 *
+	 * @return array<int, array{id: string, name: string}>|WP_Error
+	 */
+	public function list_models();
 
 	/**
 	 * @param array<string, mixed> $request Provider-neutral request.
